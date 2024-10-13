c83e8c23ac19ec1ab050a3b0d514ae8e9a0bad73
Wan1049 <wanwunep@gmail.com>
2024-10-13 22:11:36 +0700

    sammy5678

--- classes.py
+++ classes.py
@@ -5,3 +5,13 @@ import datetime
 
 load_dotenv()
 connect('techhub', host=str(os.getenv("MONGODB_URI")))
+
+class User(Document):
+
+    # 1. User Information
+    discord_id = fields.StringField(required=True)
+    user_name = fields.StringField(required=True)
+
+    # 2. Progession Data
+    level = fields.IntField(default=1)
+    exp = fields.IntField(default=0)
