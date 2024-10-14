58928a059cbd456e684146598ea5a848d4cc5853
Wan1049 <wanwunep@gmail.com>
2024-10-14 22:43:18 +0700

    123

--- gacha.py
+++ gacha.py
@@ -20,7 +20,7 @@ client = MongoClient(MONGODB_URI, server_api=ServerApi('1'))
 intents = discord.Intents.default()
 intents.members = True
 intents.voice_states = True
-intents.presences = True 
+intents.presences = True
 intents.message_content = True  
 
 class GachaView(discord.ui.View):
@@ -29,21 +29,28 @@ class GachaView(discord.ui.View):
 
     @discord.ui.button(label="Roll Gacha x1", style=discord.ButtonStyle.primary, row=0)
     async def one_roll(self, interaction: discord.Interaction, button: discord.ui.Button):
-
         gacha_result = GachaResult(user_id=interaction.user.id, discord_user=interaction.user)
+        await interaction.response.send_message("You got : ")
         await gacha_result.display_result(interaction)
 
     @discord.ui.button(label="Roll Gacha x10", style=discord.ButtonStyle.primary, row=0)
     async def ten_rolls(self, interaction: discord.Interaction, button: discord.ui.Button):
-        pass
+        gacha_result = GachaResult(user_id=interaction.user.id, discord_user=interaction.user)
+        await interaction.response.send_message("You got : ")
+        for _ in range(10):
+            await gacha_result.display_result(interaction)
 
-    @discord.ui.button(label="Check Gacha rate", style=discord.ButtonStyle.primary, row=0)
+    @discord.ui.button(label="Check Gacha Rate", style=discord.ButtonStyle.primary, row=0)
     async def show_rate(self, interaction: discord.Interaction, button: discord.ui.Button):
-
+        gacha_rate = check_rate(user_id=interaction.user.id)
+        
         embed = discord.Embed(
-                            title="*****GACHA**RATE******",
-                            description=f"**User Name:** {check_rate(user_id=interaction.user.id)} %",
-                            color=discord.Color.darker_gray())
+            title="*****GACHA RATE******",
+            description=f"**User Name:** {interaction.user.name}\n**Gacha Rate:** {gacha_rate} %",
+            color=discord.Color.darker_gray()
+        )
+        
+        await interaction.response.send_message(embed=embed)
 
 class GachaResult(discord.ui.View):
     def __init__(self, user_id, discord_user):
@@ -52,5 +59,5 @@ class GachaResult(discord.ui.View):
         self.discord_user = discord_user
 
     async def display_result(self, interaction: discord.Interaction):
-        
-        roll_result = roll_gacha()
+        result = roll_gacha(self.user_id)
+        await interaction.followup.send(result)
